f0f7755b48e46af6340c292755e8d70f1fe0b203
WireNext <wirennextt@gmail.com>
2026-03-25 23:04:48 +0100

    0.6.7

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCbzlHhGeAU_BB5eyP9T8DXP6evye5aYF4",
   authDomain: "meteocv.firebaseapp.com",
@@ -16,12 +14,12 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Esto hace que aparezca la notificación cuando la web está en segundo plano
+// Esto detecta los mensajes cuando la web NO está abierta
 messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/multimedia/logo.png', // Asegúrate que esta ruta existe
+    icon: '/multimedia/logo.png'
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
