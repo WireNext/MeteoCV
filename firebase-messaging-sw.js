1b614857e32490e1c99889968057b9e731fb5e7e
WireNext <wirennextt@gmail.com>
2026-03-27 16:42:41 +0100

    0.6.8.4

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,3 +23,26 @@ messaging.onBackgroundMessage((payload) => {
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close(); // Cierra la notificación al pinchar
+
+  // Define a qué URL quieres enviar al usuario
+  const urlToOpen = 'https://meteocv.vercel.app';
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(windowClients) {
+      // Si la web ya está abierta en una pestaña, le da el foco (la trae al frente)
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // Si no estaba abierta, abre una pestaña nueva con tu web
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
